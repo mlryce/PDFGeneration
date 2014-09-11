ca33599aa35e1b8b389c12c10688410803bd72df
Michael Ryce <mlryce@crimson.ua.edu>
2014-09-11 18:13:22 -0500

    added some classes

---
+++ ConsoleApplication1/ConsoleApplication1/classes/immunization_form.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.classes
+{
+    class immunization_form
+    {
+        
+        
+        string treatment_consent_text = "Medical Treatment Consent (For Student Under 18): "  +
+                                        "I hereby authorize " /*+ organization*/ + "to employ" +
+                                        "daignostic procedures and to render any treatment"   +
+                                        "or medical, surgial, pyschological or psychiatric "  +
+                                        "care deemed necessary to the health and well-being " +
+                                        "of my child to an accredited hospital or other "     +
+                                        "care facility if deemed necessary by the medical or" +
+                                        "mental health provider";
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/classes/organization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.classes
+{
+    class organization
+    {
+        private string organization_name;
+            public string get_organization_name(){
+                return this.organization_name;
+            }
+            public void set_organization_name(string name){
+                this.organization_name = name;
+            }
+               
+    }
+}
