820f5aeca20fa3b16847678f0208adb5598f9103
Michael Ryce <mlryce@crimson.ua.edu>
2014-09-11 18:55:07 -0500

    worked on classes

--- ConsoleApplication1/ConsoleApplication1/classes/immunization_form.cs
+++ ConsoleApplication1/ConsoleApplication1/classes/immunization_form.cs
@@ -6,17 +6,20 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1.classes
 {
-    class immunization_form
+    public class immunization_form
     {
+        public static organization asdf = new organization();
         
-        
-        string treatment_consent_text = "Medical Treatment Consent (For Student Under 18): "  +
-                                        "I hereby authorize " /*+ organization*/ + "to employ" +
-                                        "daignostic procedures and to render any treatment"   +
-                                        "or medical, surgial, pyschological or psychiatric "  +
-                                        "care deemed necessary to the health and well-being " +
-                                        "of my child to an accredited hospital or other "     +
-                                        "care facility if deemed necessary by the medical or" +
+        public string treatment_consent_text = "Medical Treatment Consent (For Student Under 18): "  +
+                                        "I hereby authorize " + asdf.get_organization_name()         +
+                                        "daignostic procedures and to render any treatment"          +
+                                        "or medical, surgial, pyschological or psychiatric "         +
+                                        "care deemed necessary to the health and well-being "        +
+                                        "of my child to an accredited hospital or other "            +
+                                        "care facility if deemed necessary by the medical or"        +
                                         "mental health provider";
     }
+    
+
 }
+
