321bf3353d5b70d0bf8c83efc2093a255e75a8ea
Michael Ryce <mlryce@crimson.ua.edu>
2014-09-15 13:50:02 -0500

    changed code slightly

--- ConsoleApplication1/ConsoleApplication1/classes/immunization_form.cs
+++ ConsoleApplication1/ConsoleApplication1/classes/immunization_form.cs
@@ -8,14 +8,14 @@ namespace ConsoleApplication1.classes
 {
     public class immunization_form
     {
-        public static organization asdf;
+        public static organization school;
         public string treatment_consent_text;
         
 
-        public immunization_form(organization qwer)
+        public immunization_form(organization org)
         {
-            asdf = qwer;
-            treatment_consent_text = "Medical Treatment Consent (For Student Under 18): I hereby authorize " + qwer.get_organization_name() + " diagnostic procedures and to " +
+            school = org;
+            treatment_consent_text = "Medical Treatment Consent (For Student Under 18): I hereby authorize " + org.get_organization_name() + " diagnostic procedures and to " +
                                      "render any treatment or medical, surgial, pyschological or psychiatric care deemed necessary to the health and well-being of my child.  I grant " +
                                      "permission for the transfer of my child to an accredited hospital or other health care facility if deemed necessary by the medical or mental " +
                                      "health provider.";
