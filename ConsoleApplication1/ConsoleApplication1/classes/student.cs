0723da34041a0afc8a3b76704b30f536d84dc00a
akray <akray@crimson.ua.edu>
2014-09-15 14:52:09 -0500

    Resolution Commit

--- ConsoleApplication1/ConsoleApplication1/classes/student.cs
+++ ConsoleApplication1/ConsoleApplication1/classes/student.cs
@@ -3,46 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace ConsoleApplication1.classes
 {
-    public class student
+    public class Student
     {
-        string student_first_name;
-        string student_last_name;
-        DateTime student_dob;
-        organization student_school;
+        public int studentID { get; set; }
+        public string student_first_name { get; set; }
+        public string student_last_name { get; set; }
+        public DateTime student_dob { get; set; }
+        public organization student_school { get; set; }
         
 
-            public string get_student_name()
-            {
-                return this.student_first_name + " " + this.student_last_name;
-            }
-            public void set_student_first_name(string fn)
-            {
-                this.student_first_name = fn;
-            }
-            public void set_student_last_name(string ln)
-            {
-                this.student_last_name = ln;
-            }
+            //public string get_student_name()
+            //{
+            //    return this.student_first_name + " " + this.student_last_name;
+            //}
+            //public void set_student_first_name(string fn)
+            //{
+            //    this.student_first_name = fn;
+            //}
+            //public void set_student_last_name(string ln)
+            //{
+            //    this.student_last_name = ln;
+            //}
 
-     public student()
-     {
-         student_first_name = "Bob";
-         student_last_name = "Smith";
-         student_dob = DateTime.Now;
-         student_school = null;
-     }
+     //public Student()
+     //{
+     //    student_first_name = "Bob";
+     //    student_last_name = "Smith";
+     //    student_dob = DateTime.Now;
+     //    student_school = null;
+     //}
 
-     public student(string fn, string ln, DateTime dob, organization school)
-     {
-         student_first_name = fn;
-         student_last_name = ln;
-         student_dob = dob;
-         student_school = school;
-     }
+     //public Student(string fn, string ln, DateTime dob, organization school)
+     //{
+     //    student_first_name = fn;
+     //    student_last_name = ln;
+     //    student_dob = dob;
+     //    student_school = school;
+     //}
 
         
     }
